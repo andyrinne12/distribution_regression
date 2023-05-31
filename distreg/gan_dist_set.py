4e73edce236d00b37eccfc6dd09783dea7457aad
Andrei Sologon <andysolo2000@gmail.com>
2023-05-31 14:57:07 +0100

    univar gan setup

--- distreg/gan_dist_set.py
+++ distreg/gan_dist_set.py
@@ -9,12 +9,16 @@ class GanDistributionSet:
         kme_encoder: RffEncoder,
         reg_encoder: RffEncoder,
         ground_samples: torch.Tensor,
+        noise_dists: UnivarDistribution,
         fixed_noise_dists: UnivarDistribution,
+        samples_per_dist: int,
     ):
         self.kme_encoder = kme_encoder
         self.reg_encoder = reg_encoder
         self.ground_samples = ground_samples
         self.fixed_noise_dists = fixed_noise_dists
+        self.noise_dists = noise_dists
+        self.samples_per_dist = samples_per_dist
 
         self.dist_features = torch.empty((1, reg_encoder.n_features))
         self.labels = torch.empty(1)
@@ -31,7 +35,9 @@ class GanDistributionSet:
     def encode_and_append(self, samples, label):
         features = self.encode(samples)
         self.dist_features = torch.cat((self.dist_features, features), 0)
-        self.labels = torch.cat((self.labels, torch.Tensor([label])), 0)
+        self.labels = torch.cat(
+            (self.labels, torch.Tensor([label] * features.shape[0])), 0
+        )
 
     def encode(self, samples):
         mean_embedding = self.kme_encoder.gen_mean_embeddings_from_tensor(
@@ -41,6 +47,9 @@ class GanDistributionSet:
             mean_embedding, single=True
         )
         return features
+
+    def resample_noise(self):
+        self.noise_dists.sample(self.samples_per_dist)
 
     @property
     def loss_weights(self):
--- distreg/models/generator.py
+++ distreg/models/generator.py
@@ -1,23 +1,33 @@
+from collections import deque
+import numpy as np
+
 import seaborn as sns
 import torch
 from matplotlib import pyplot as plt
 from torch import nn
 
 from distreg.gan_dist_set import GanDistributionSet
+from distreg.models.classifier import Classifier
+
+
+def roundl(arr, dec):
+    return [round(n, dec) for n in arr]
+
+
+def absl(arr):
+    return [abs(n) for n in arr]
 
 
 class Generator(nn.Module):
     def __init__(self, input_dim, output_dim=1, bias=False):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Linear(input_dim, 64, bias=bias),
-            nn.ReLU(),
-            nn.Linear(64, 32, bias=bias),
-            nn.ReLU(),
+            nn.Linear(input_dim, 32, bias=bias),
+            nn.GELU(),
             nn.Linear(32, 16, bias=bias),
-            nn.ReLU(),
+            nn.GELU(),
             nn.Linear(16, 8, bias=bias),
-            nn.ReLU(),
+            nn.GELU(),
             nn.Linear(8, output_dim, bias=bias),
         )
 
@@ -30,23 +40,46 @@ def train_gan(
     gan_dist_set: GanDistributionSet,
     classifier,
     classifier_opt,
+    classifier_lr,
     generator: Generator,
     generator_opt,
+    batch_size,
+    n_features,
     epochs,
     classifier_subepochs,
     generator_subepochs,
     device,
     verbose=False,
+    save_plots=False,
 ):
     gen_losses = []
     gen_preds = []
-    noise_dists = gan_dist_set.fixed_noise_dists
+    fixed_noise_dists = gan_dist_set.fixed_noise_dists
+    noise_dists = gan_dist_set.noise_dists
 
-    true_labels = torch.ones((noise_dists.n_distributions, 1))
+    true_labels = torch.ones((noise_dists.n_distributions, 1)).flatten()
+    means = [torch.mean(gan_dist_set.ground_samples).item()]
+    stds = [torch.std(gan_dist_set.ground_samples).item()]
+    hues = [1.0]
 
     for e in range(1, epochs + 1):
+        for g in generator_opt.param_groups:
+            g["lr"] = g["lr"] * 0.96
+            print(f"LR: {g['lr']}")
+
+        classifier = Classifier(n_features).to(device)
+        classifier_opt = torch.optim.LBFGS(
+            classifier.parameters(), lr=classifier_lr
+        )
+
+        gan_dist_set.resample_noise()
         output_sample = generator(noise_dists.samples).detach()
-        gan_dist_set.encode_and_append(output_sample, 0)
+
+        means.append(torch.mean(output_sample).item())
+        stds.append(torch.std(output_sample).item())
+        hues.append(0.0)
+
+        gan_dist_set.encode_and_append(output_sample[:5], 0)
 
         classifier_criterion = nn.BCELoss(
             reduction="mean"
@@ -54,6 +87,11 @@ def train_gan(
         )
         data = gan_dist_set.dist_features
         labels = gan_dist_set.labels
+
+        print("Classifier:")
+
+        losses_queue = deque(maxlen=5)
+        last_loss = 0.0
         for se in range(1, classifier_subepochs + 1):
 
             def closure():
@@ -68,29 +106,62 @@ def train_gan(
 
             y = classifier(data).flatten()
             loss = classifier_criterion(y, labels)
-            if se % 200 == 0 and verbose:
-                print(f"Classif loss: {round(loss.item(), 3)}")
 
-        generator_criterion = nn.BCELoss(reduction="sum")
-        for se in range(1, generator_subepochs + 1):
-            generator.zero_grad()
-            generator_opt.zero_grad()
-
-            output_sample = generator(noise_dists.samples)
-            features = gan_dist_set.encode(output_sample)
-            y = classifier(features)
-            loss = generator_criterion(y, true_labels)
-            loss.backward()
-
-            # for p in generator.parameters():
-            #     print(p.grad)
-
-            generator_opt.step()
-
-            if se % 400 == 0 and verbose:
+            losses_queue.appendleft(loss.item() - last_loss)
+            last_loss = loss.item()
+            if max(absl(list(losses_queue))) < 1e-3:
                 print(
-                    f"Gen pred: {round(y[0].item(), 3)}, loss: "
-                    f"{round(loss.item(), 3)}"
+                    f"    SE {se}: pred: {round(y[0].item(), 3)}, "
+                    f"loss: {round(loss.item(), 3)}"
+                )
+                break
+
+            if se % 50 == 0 and verbose:
+                print(
+                    f"    SE {se}: pred: {roundl(y.tolist(), 3)}, "
+                    f"loss: {round(loss.item(), 3)}"
+                )
+
+        print("Generator:")
+        generator_criterion = nn.BCELoss(reduction="sum")
+
+        losses_queue = deque(maxlen=3)
+        last_loss = 0.0
+        for se in range(1, generator_subepochs + 1):
+            total_loss = 0.0
+            train_size = noise_dists.samples.shape[0]
+            for batch_id in range(train_size // batch_size):
+                idx = np.arange(
+                    batch_id * batch_size,
+                    min((batch_id + 1) * batch_size, train_size),
+                )
+                generator.zero_grad()
+                generator_opt.zero_grad()
+
+                output_sample = generator(noise_dists.samples[idx])
+                features = gan_dist_set.encode(output_sample)
+
+                y = classifier(features).flatten()
+                loss = generator_criterion(y, true_labels[idx])
+
+                loss.backward()
+                generator_opt.step()
+
+                total_loss += loss.item()
+
+            losses_queue.appendleft(total_loss - last_loss)
+            last_loss = total_loss
+            if max(absl(list(losses_queue))) < 1e-2:
+                print(
+                    f"    SE {se}: pred: {round(y[0].item(), 3)}, "
+                    f"loss: {round(total_loss, 3)}"
+                )
+                break
+
+            if (se == 1 or se % 5 == 0) and verbose:
+                print(
+                    f"    SE {se}: pred: {round(y[0].item(), 3)}, "
+                    f"loss: {round(total_loss, 3)}"
                 )
 
         gen_losses.append(loss.item())
@@ -110,14 +181,15 @@ def train_gan(
                 )
             )
 
-            _, axs = plt.subplots(1, 2, figsize=(8, 4))
-            # axs[0].set_xlim(0, 400)
-            # axs[1].set_xlim(0, 400)
-            sns.histplot(output_sample[0].detach().numpy(), ax=axs[0])
+            fixed_output_sample = generator(fixed_noise_dists.samples)
+            fig, axs = plt.subplots(1, 2, figsize=(6, 3))
+            sns.histplot(fixed_output_sample[0].detach().numpy(), ax=axs[0])
             sns.histplot(
                 gan_dist_set.ground_samples[0].detach().numpy(), ax=axs[1]
             )
             plt.show()
+            if save_plots:
+                fig.savefig(f"train_plots/histplot_{e}.png")
 
 
 def train_generator_only(

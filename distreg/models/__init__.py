2943bad1d727344ce6c0101bf15ec8dab5691c70
Andrei Sologon <andysolo2000@gmail.com>
2023-06-23 06:42:06 +0100

    refined draft

--- distreg/models/__init__.py
+++ distreg/models/__init__.py
@@ -1,10 +1,9 @@
 from distreg.models.classifier import Classifier, train_classifier
-from distreg.models.generator import Generator, train_generator_only, train_gan
+from distreg.models.generator import Generator, train_gan
 
 __all__ = [
     "Classifier",
     "Generator",
     "train_classifier",
     "train_gan",
-    "train_generator_only",
 ]
